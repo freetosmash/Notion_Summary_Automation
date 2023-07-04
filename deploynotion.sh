068d5649835a016b4a76a91b2bb35cbd34477368
Rah Xu <33079010+freetosmash@users.noreply.github.com>
2023-07-04 19:54:33 +0800

    Add files via upload

--- deploynotion.sh
+++ deploynotion.sh
@@ -3,7 +3,7 @@
 curl https://raw.githubusercontent.com/freetosmash/Notion_Summary_Automation/main/notion_automation.js -o notion_automation.js
 
 read -p "Enter your Notion API key: " notionApiKey
-read -p "Enter your OpenAI API key: " openaiApiKey
+read -p "Enter your OpenAI/Azure OpenAI API key: " openaiApiKey
 read -p "Enter the summarization API (openai or azure): " summarizationApi
 echo "Enter the database IDs (press enter when finished):"
 databaseIds=""
